13e8cdc3b2d86dba88a0eb624795604d1c836ed6
sstacey <shaun.stacey1@gmail.com>
2021-07-14 07:48:43 -0500

    Cleanup local article state code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
+
 import { fetchArticlesBySub } from '../src/features/article/articleSlice'
 import Article from '../src/features/article/Article'
-
-import Reddit from './utils/Reddit'
 
 function App() {
 
